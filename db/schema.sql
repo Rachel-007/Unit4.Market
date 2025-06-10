1d18e2dcaf51e780070a65a9fb44477c4fab822a
Rachel Davis <rachelddavis@aol.com>
2025-06-10 10:51:52 -0600

    Unit4.Market Updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,45 @@
 -- TODO
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS orders cascade;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users cascade;
+-- This file defines the schema for the database.
+
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  date DATE NOT NULL,
+  note TEXT,
+  user_id INTEGER NOT NULL
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  price DECIMAL NOT NULL
+);
+
+CREATE TABLE orders_products (
+  order_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  quantity INTEGER NOT NULL
+);
+
+-- Ref: users.id < orders.user_id [delete: cascade]
+CREATE INDEX idx_orders_products_order_id ON orders_products(order_id);
+CREATE INDEX idx_orders_products_product_id ON orders_products(product_id);
+
+
+
+-- Ref: users.id < orders.user_id [delete: cascade]
+-- Ref: orders.id < orders_products.order_id [delete: cascade]
+-- Ref: products.id < orders_products.product_id [delete: cascade]
+
